8da022ba3bd6044ddcdca302b63fa584620b63ed
Niko Savola <niko.savola@aalto.fi>
2021-03-28 18:46:52 +0300

    Working predictions

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Button from 'react-bootstrap/Button'
-// import './Score.css'
+import * as tf from '@tensorflow/tfjs'
 
 const Score = () => {
-  const score = 0.84 * 100
+  const [model, setModel] = useState()
+  const [score, setScore] = useState(0.0)
+
+  // Load TensorFlow model on first render
+  useEffect(async () => {
+    const newModel = await tf.loadLayersModel('https://raw.githubusercontent.com/NikoDaGreat/video-game-rating-ml/main/public/model/model.json')
+    setModel(newModel)
+  }, [])
+
+  const handlePredict = (event) => {
+    event.preventDefault()
+
+    const vector = Array(378).fill(0) // tf.zeros([1, 378])
+    const list = [0, 3]
+    list.forEach((index) => {
+      vector.splice(index, 1, 1)
+    })
+
+    const tensor = tf.tensor2d(vector, [1, 378], 'int32')
+    const prediction = model.predict(tensor)
+    setScore((prediction.dataSync()[0] * 100).toFixed(2))
+  }
 
   return (
     <>
@@ -14,7 +35,7 @@ const Score = () => {
         {' '}
         %
       </p>
-      <Button variant="primary" size="xxl">
+      <Button variant="primary" size="xxl" onClick={handlePredict}>
         Predict
       </Button>
     </>
